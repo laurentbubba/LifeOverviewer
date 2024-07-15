f62209f9c070112e991286027ec73eb80961ae4f
laurent <laurent.bubba@hotmail.com>
2024-07-15 18:30:26 +0200

    re-enabled secret key

--- life_overview/settings/production.py
+++ life_overview/settings/production.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # Would like to turn on for best practices
-# from life_overview.settings import get_secret
-# SECRET_KEY = get_secret('SECRET_KEY')
+from life_overview.settings import get_secret
+SECRET_KEY = get_secret('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Now like production
