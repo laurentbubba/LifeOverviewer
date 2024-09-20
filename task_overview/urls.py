f653b8340bf611342f914f9643b528afd2a5bec6
laurent <laurent.bubba@hotmail.com>
2024-09-20 21:54:22 +0200

    to check workflow

--- task_overview/urls.py
+++ task_overview/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from task_overview import views
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("tasks", views.task_overview, name="tasks")
 ]
--- task_overview/views.py
+++ task_overview/views.py
@@ -1,8 +1,14 @@
 from django.http import HttpResponse
+from .models import Task
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
     return HttpResponse("I love my Genta! A BUNCH")
-    # return HttpResponse("Here will be the task and their inheritances overview ...")
+
+def task_overview(request):
+    all_tasks = Task.objects.all()
+    context = {"all_tasks":Task.objects.all()}
+
+    return render(request, '../templates/tasks.html', context)
